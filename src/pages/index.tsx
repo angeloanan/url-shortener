1e0cd4b6d6e20ccde3d264a07f8d9d9e5b856ac8
Christopher Angelo <angelo@angeloanan.xyz>
2021-03-01 18:52:02 +0700

    feat(index-page): added more links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,27 @@ import Link from 'next/link'
 const IndexPage = () => {
   return (
     <>
-      <h1>Angelo's URL shortener</h1>
+      <h1 style={{ fontWeight: 700 }}>Angelo's URL shortener</h1>
       <p>Here, you can find a very fancy link such as:</p>
       <ul>
         <li>
-          <Link href='/twitter' passHref>
-            <a>/twitter</a>
+          <Link href='/wob' passHref>
+            <a>/wob</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/duck' passHref>
+            <a>/duck</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/.env' passHref>
+            <a>/.env</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/ActualWebsite' passHref>
+            <a>/ActualWebsite</a>
           </Link>
         </li>
       </ul>
